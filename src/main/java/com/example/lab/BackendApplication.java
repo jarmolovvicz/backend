1596884efc800813c630f9f9ddeab282202195fb
Damian <damian.jarmolowicz@outlook.com>
2024-10-14 22:06:46 +0200

    1

---
+++ src/main/java/com/example/lab/BackendApplication.java
@@ -0,0 +1,26 @@
+package com.example.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("rest")
+class TestController {
+
+	@GetMapping("test")
+	public String testEndpoint() {
+		return "Hello World!";
+		}
+	}
